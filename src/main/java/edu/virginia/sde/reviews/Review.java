dc8b2d2103bba613caf3df35e4635588706e7bdd
ssmatt4 <ssmatthew2004@gmail.com>
2023-11-27 21:28:30 -0500

    coded Review class and created course-reviews.fxml and CourseReviewsController.java

---
+++ src/main/java/edu/virginia/sde/reviews/Review.java
@@ -0,0 +1,40 @@
+package edu.virginia.sde.reviews;
+
+import java.sql.Time;
+import java.sql.Timestamp;
+
+public class Review {
+    private Course course;
+    private int rating;
+    private Timestamp timestamp;
+    private String comment;
+
+    public Review(Course course) {
+        this.course = course;
+    }
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+    public int getRating() {
+        return rating;
+    }
+    public void setTimeStamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+    public void setComment(String string) {
+        this.comment = string;
+    }
+    public String getComment() {
+        return comment;
+    }
+
+    public Boolean hasComment() {
+        if (comment.equals("")) {
+            return false;
+        }
+        return true;
+    }
+}
