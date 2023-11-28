59cffad36710f295f6d915751d02fa23642a62d2
Jamie <pby5br@virginia.edu>
2023-11-27 21:35:46 -0500

    basic search bar display

--- src/main/java/edu/virginia/sde/reviews/CourseSearchController.java
+++ src/main/java/edu/virginia/sde/reviews/CourseSearchController.java
@@ -3,6 +3,7 @@ package edu.virginia.sde.reviews;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 import javax.swing.text.LabelView;
 
@@ -11,6 +12,12 @@ public class CourseSearchController {
     private TextField searchField;
     @FXML
     private Label messageLabel;
+    private Stage stage;
+
+    public void setStage(Stage stage){
+        this.stage = stage;
+    }
+
     @FXML
     private void handleSearch(){
         String course = searchField.getText();
