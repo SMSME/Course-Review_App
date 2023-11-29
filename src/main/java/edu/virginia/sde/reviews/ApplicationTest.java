93adc7ef30dabd53e2839d76cd4eb4c8a81bc25a
ssmatt4 <ssmatthew2004@gmail.com>
2023-11-28 19:14:19 -0500

    renamed file

--- src/main/java/edu/virginia/sde/reviews/ApplicationTest.java
+++ src/main/java/edu/virginia/sde/reviews/ApplicationTest.java
@@ -4,7 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-public class CourseReviewsApplication extends Application{
+public class ApplicationTest extends Application{
     public static void main(String[] args) {
         launch(args);
     }
