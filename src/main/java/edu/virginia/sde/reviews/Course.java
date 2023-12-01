559b234487a33be1d3f93747b2fdd59d2878eead
Jamie <pby5br@virginia.edu>
2023-12-01 11:04:56 -0500

    adding review section to course?? - also got deleted wat

--- src/main/java/edu/virginia/sde/reviews/Course.java
+++ src/main/java/edu/virginia/sde/reviews/Course.java
@@ -1,15 +1,20 @@
 package edu.virginia.sde.reviews;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Course {
     private String courseSubject;
     private String courseTitle;
     private int courseNumber;
+    private List<Review> reviews;
 
 
     public Course(String courseSubject, int courseNumber, String courseTitle){
         this.courseSubject = courseSubject;
         this.courseNumber = courseNumber;
         this.courseTitle = courseTitle;
+        this.reviews = new ArrayList<>();
     }
 
     public String getCourseSubject() {
@@ -36,15 +41,31 @@ public class Course {
         this.courseNumber = courseNumber;
     }
 
+    public List<Review> getReviews(){
+        return reviews;
+    }
+
+    public void addReview(Review review){
+        reviews.add(review);
+    }
+
+    public double getAvgRating(){
+        if (reviews.isEmpty()){
+            return 0.0;
+        }
+        double sum = 0.0;
+        for (Review review: reviews){
+            sum += review.getRating();
+        }
+        return sum/reviews.size();
+    }
+
     @Override
     public String toString() {
         return "Course{" +
                 "subject='" + courseSubject + '\'' +
                 ", number=" + courseNumber +
                 ", title='" + courseTitle + '\'' +
+                ", reviews=" + reviews +
                 '}';
-    }
-
-
-
-}
+    }}
